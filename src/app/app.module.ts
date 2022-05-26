0ee8c6418de617b315897966b42a45742c662a8a
RodrigoProgram22 <rodrigoprograma22@gmail.com>
2022-05-26 17:39:08 -0300

    Se ajusto el navbar y el footer, y se agrego el home y la seccion de productos-destacados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProductosDestacadosComponent } from './components/section/productos-destacados/productos-destacados.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, FooterComponent],
+  declarations: [AppComponent, FooterComponent, HomeComponent, ProductosDestacadosComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
--- src/assets/app.js
+++ src/assets/app.js
@@ -1,5 +1,6 @@
 window.addEventListener("scroll", function () {
   var nav = document.querySelector("nav");
   nav.classList.remove("bg-transparent");
-  nav.classList.toggle("fondo-dark", window.scrollY > 0);
+  nav.classList.toggle("fondo-claro", window.scrollY > 2);
+  nav.classList.toggle("fixed-top", window.scrollY > 2);
 });
